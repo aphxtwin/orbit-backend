f104e0e113c8186e070d2df35d9f7521587daee0
aphxtwin <ivospironello@gmail.com>
2025-09-25 17:43:12 -0300

    fix: update CORS configuration to use HTTPS for frontend URL

--- src/index.js
+++ src/index.js
@@ -39,7 +39,7 @@ app.use(cors({
     process.env.FRONT_URL || 'http://localhost:3000', 
     'http://localhost:3001', 
     'http://localhost:8080',
-    'orbit-five-teal.vercel.app',
+    'https://orbit-five-teal.vercel.app',
     'https://orbitg.bici-dev.com', 
     'https://orbit.bici-dev.com', 
     'https://apig.bici-dev.com',
